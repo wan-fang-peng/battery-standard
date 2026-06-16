2e7fe77aa22cc40bba39cac03b5d9a904750fec4
wan-fang-peng <3916796092@qq.com>
2026-06-16 23:15:02 +0800

    Delete src/app/page.tsx

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatInterface from "@/components/chat";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <ChatInterface />
-    </main>
-  );
-}
