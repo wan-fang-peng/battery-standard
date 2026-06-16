eec7e5b1fe5b5e032f41298eb2ea7650e43ac9e0
wan-fang-peng <3916796092@qq.com>
2026-06-16 23:36:08 +0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>电池适航标准智能搜索引擎</h1>
+      <p>基于 RAG + 知识图谱的智能搜索系统</p>
+    </div>
+  );
+}
