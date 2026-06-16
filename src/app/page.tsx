35c68671c65563a45e7171ced1c3732d10a22da3
wan-fang-peng <3916796092@qq.com>
2026-06-16 15:36:06 +0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ChatInterface from "@/components/chat";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <ChatInterface />
+    </main>
+  );
+}
